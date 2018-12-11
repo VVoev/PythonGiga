9308042b7b618cf78928594e537d6dfdd48e882f
vladovoev <vladovoev@yahoo.co.uk>
2018-12-11 14:58:20 +0200

    Webmap

--- 02.Webmap/app.py
+++ 02.Webmap/app.py
@@ -1,33 +1,34 @@
 import folium
 import pandas
+import json
+
+df = pandas.read_csv('./cordinates.txt')
 
 
-def color_provider(elev):
-    color = ''
-    if elev <= 1000:
-        color = 'green'
-    elif 1000 < elev and elev < 3000:
-        color = 'orange'
+def colors(elev):
+    minimum = int(min(df['ELEV']))
+    step = int(max((df['ELEV'])-min(df['ELEV']))/3)
+    if elev in range(minimum, minimum+step):
+        col = "green"
+    elif elev in range(minimum+step, minimum+step*2):
+        col = "orange"
     else:
-        color = 'red'
-    return color
+        col = "red"
+    return col
 
 
-data = pandas.read_csv('./cordinates.txt')
-lat = list(data['LAT'])
-lon = list(data['LON'])
-elev = list(data['ELEV'])
+map_1 = folium.Map(location=[df['LAT'].mean(), df['LON'].mean()],
+                   zoom_start=6, tiles='mapbox bright')
 
-map = folium.Map(location=[38.59, -99.09],
-                 zoom_start=6, tiles='Mapbox Bright')
+for name, lon, lat, elev in zip(df['NAME'], df['LON'], df['LAT'],
+                                df['ELEV']):
+    folium.Marker([lat, lon], popup=name,
+                  icon=folium.Icon(color=colors(elev))).add_to(map_1)
 
-fg = folium.FeatureGroup(name='My Map')
-for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(
-        location=[lt, ln], popup=str(el)+' m', radius=6, fill_color=color_provider(el), color='grey', fill_opacity=0.7))
-
-fg.add_child(folium.GeoJson(
-    data=open('world.json', 'r', encoding='utf-8-sig')))
-
-map.add_child(fg)
-map.save('Map.html')
+folium.GeoJson(open('./world.json'),
+               name='geojson',
+               style_function=lambda x: {'fillColor': 'green' if
+                                         x['properties']['POP2005'] < 10000000
+                                         else 'orange' if 10000000 < x['properties']['POP2005'] > 20000000 else 'red'}
+               ).add_to(map_1)
+folium.LayerControl().add_to(map_1)
